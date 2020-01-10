7820e0a8724f50bf93ba9acfd91b061421c77f58
Michael Rivera <mirivera@student.42.us.org>
2020-01-10 11:48:22 -0800

    rewrote move script to be correct, BUT it still just doesn't seem to want to work, I'm not too sure how to fix it so time to move on to actual exercises

--- 00_how_to_docker/move_docker_goinfre.sh
+++ 00_how_to_docker/move_docker_goinfre.sh
@@ -1,11 +1,12 @@
 # This script runs the bash command to move the .docker file into goinfre
 
-# This script must be run BEFORE you create your docker machine!
+# This script must be run AFTER you create and rm a docker machine!
 
 # You can cd to other places and perform actions, however,
 # once those actions have been completed, the terminal instance
 # is still inside the directory you ran the command from
 
+# NC=`tput setaf 0`
 red=`tput setaf 1`
 green=`tput setaf 2`
 yellow=`tput setaf 3`
@@ -16,16 +17,19 @@ cd ~/goinfre
 if [ -d ".docker" ]; then
     printf "${blue}The .docker file is already inside goinfre\n"
 else    
-    cd
+    # cd
     if [ -d ".docker" ]; then
-    printf "${green}Moving .dockerdir to goinfre\n\n"
+		printf "${green}Moving .dockerdir to goinfre\n\n"
     fi
-    # move docker folder to goinfre
-    mv .docker goinfre
-    cd goinfre
     # create a symbolic link to goinfre/.docker called .docker
-    ln -s goinfre/.docker ../.docker
-    # # list out the inodes link
-    printf "${green}Link created.\n"
-    ls -ila .docker
+	# ln -s /path/to/original/ /path/to/linkName
+	mkdir .docker
+    ln -sF goinfre/.docker/ ~/
+    # ln -sf goinfre/.docker/ ~/.docker
+    # list out the inodes link
+    printf "${green}Link created.${NC}\n"
+	# CURDIR="$(pwd)"
+    # printf "${blue}You're current directory is $CURDIR.\n"
+	cd ..
+    ls -ila .docker/
 fi
