4a83d02cebc7089ad7d083fa261d6222eb6b078b
Michael Rivera <mirivera@student.42.us.org>
2020-01-31 12:36:58 -0800

    made revisions, going to lunch, will try to run docker containers on another computer outside of zone 3 later

--- 01_dockerfiles/ex01/run_me.sh
+++ 01_dockerfiles/ex01/run_me.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+black=`tput setaf 0`
+mint=`tput setaf 10`
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+bold=`tput bold`
+# tput sgr0 turns off all attributes
+NC=`tput sgr0`
 
 # run dockerfile, label image as my_serv, build without the cache
 docker build -t my_serv --no-cache .
@@ -15,9 +24,13 @@ wget https://files.teamspeak-services.com/releases/client/3.3.0/TeamSpeak3-Clien
 open TeamSpeak3-Client-macosx-3.3.0.dmg
 read -p "Press enter after you've agreed to the license and the dmg window has opened."
 cd /Volumes/TeamSpeak\ 3\ Client
-cp -R TeamSpeak\ 3\ Client.app ~/Desktop
+cp -R /Volumes/TeamSpeak\ 3\ Client/TeamSpeak\ 3\ Client.app ~/Desktop
 cd ~/Desktop
 open TeamSpeak\ 3\ Client.app 
+
+# copy the ip address to the clipboard
+docker-machine ip Char | pbcopy 
+echo "${red}IP of the Virtual Machine has been copied to the clipboard. Paste it in the server nickname/address field${NC}"
 
 # to show the admin privilege key for copy paste
 docker logs teamspeak
